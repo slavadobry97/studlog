401768b2c5c4a16bc28efd35126885992923eba1
slavadobry97 <slavadobry97@gmail.com>
2026-02-07 21:16:22 +0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withPWA(nextConfig);
